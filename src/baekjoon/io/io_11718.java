3f33113f7a8531f52090d721210d2853e9c81a05
ChaewonHan <chwonhan00@gmail.com>
2021-06-11 21:41:04 +0900

    백준 11718

--- src/baekjoon/io/io_11718.java
+++ src/baekjoon/io/io_11718.java
@@ -6,8 +6,8 @@ public class io_11718 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while(sc.hasNextLine()) {
-            String input = sc.nextLine();
-            System.out.println(input);
+            String str = sc.nextLine();
+            System.out.println(str);
         }
         sc.close();
     }
