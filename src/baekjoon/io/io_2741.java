7ca958c3ada3d4ac2647a237eee9e6cd15128375
ChaewonHan <chwonhan00@gmail.com>
2021-06-16 15:36:10 +0900

    백준 2792, 2743, 2739

---
+++ src/baekjoon/io/io_2741.java
@@ -0,0 +1,15 @@
+package baekjoon.io;
+
+import java.util.Scanner;
+
+public class io_2741 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for(int i=1; i <= num; i++){
+            System.out.println(i);
+        }
+    }
+}
