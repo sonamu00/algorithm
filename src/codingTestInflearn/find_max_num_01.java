8172f75aa71b1b38ccfaf962cc73e5ef6a96056a
ChaewonHan <chwonhan00@gmail.com>
2025-01-08 21:42:57 +0900

    codingTestInflearn Week1 01,02

---
+++ src/codingTestInflearn/find_max_num_01.java
@@ -0,0 +1,21 @@
+package codingTestInflearn;
+
+public class find_max_num_01 {
+
+    public int findMaxNum(int nums[]) {
+        int maxNum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > maxNum) {
+                maxNum = nums[i];
+            }
+        }
+        return maxNum;
+    }
+
+    public void main(String[] args) {
+        System.out.println("정답 = 6 / 현재 풀이 값 = " + findMaxNum(new int[]{3, 5, 6, 1, 2, 4}));
+        System.out.println("정답 = 6 / 현재 풀이 값 = " + findMaxNum(new int[]{6, 6, 6}));
+        System.out.println("정답 = 1888 / 현재 풀이 값 = " + findMaxNum(new int[]{6, 9, 2, 7, 1888}));
+    }
+}
---
+++ src/codingTestInflearn/find_max_occurred_alphabet_02.java
@@ -0,0 +1,38 @@
+package codingTestInflearn;
+
+import java.util.*;
+
+public class find_max_occurred_alphabet_02 {
+    public static Character result(String strValue) {
+        Character result = null;
+        int max = 0;
+        char[] chars = strValue.replaceAll("\\s", "").toCharArray();
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < chars.length; i++) {
+            int num = 0;
+
+            if (map.containsKey(chars[i])) {
+                map.put(chars[i], map.get(chars[i]).intValue() + 1);
+            } else {
+                map.put(chars[i], num);
+            }
+
+        }
+
+        for (Character key : map.keySet()) {
+            if (map.get(key) > max) {
+                max = map.get(key);
+                result = key;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("정답 = i 현재 풀이 값 =" + result("hello my name is dingcodingco"));
+        System.out.println("정답 = e 현재 풀이 값 =" + result("we love algorithm"));
+        System.out.println("정답 = b 현재 풀이 값 =" + result("best of best youtube"));
+    }
+}
