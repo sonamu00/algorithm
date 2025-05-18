7cde8d3ad90b9610786429b46f9c7dccf547f9aa
ChaewonHan <chwonhan00@gmail.com>
2025-05-19 08:08:36 +0900

    codingTestInflearn Week2 linked list - 01

---
+++ src/codingTestInflearn/week2/append_linked_list_02_02.java
@@ -0,0 +1,27 @@
+package codingTestInflearn.week2;
+
+import java.util.*;
+
+public class append_linked_list_02_02 {
+
+
+    public append_linked_list_02_02() {}
+
+    public append_linked_list_02_02(int value) {
+
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> head;
+        Node<E> node;
+
+        Node(Node<E> head, E item, Node<E> node) {
+           this.head = head;
+           this.item = item;
+           this.node = node;
+        }
+
+    }
+
+}
