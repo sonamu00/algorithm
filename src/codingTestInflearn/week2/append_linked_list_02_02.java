a94c1930d22309cc9174c69c6cea2f927abf7f71
ChaewonHan <chwonhan00@gmail.com>
2025-07-23 07:57:04 +0900

    codingTestInflearn Week2 linked list - 02 java

--- src/codingTestInflearn/week2/append_linked_list_02_02.java
+++ src/codingTestInflearn/week2/append_linked_list_02_02.java
@@ -2,30 +2,55 @@ package codingTestInflearn.week2;
 
 import java.util.*;
 
+class LinkedListAppend {
+
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        public Node (int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public LinkedListAppend(int value) {
+        this.head = new Node(value);
+    }
+
+    public void append(int value) {
+        Node cur = this.head;
+        while (cur.next != null) {
+            cur = cur.next;
+        }
+        cur.next = new Node(value);
+    }
+
+    public void printAll() {
+        Node cur = this.head;
+        while (cur != null) {
+            System.out.println(cur.data);
+            cur = cur.next;
+        }
+    }
+
+}
+
 public class append_linked_list_02_02 {
 
-
-    public append_linked_list_02_02() {}
-
-    public append_linked_list_02_02(int value) {
-
-    }
-
-    private static class Node<E> {
-        E item;
-        Node<E> head;
-        Node<E> node;
-
-        Node(Node<E> head, E item, Node<E> node) {
-           this.head = head;
-           this.item = item;
-           this.node = node;
-        }
-
-    }
-
     public static void main(String[] args) {
+        LinkedListAppend.Node node = new LinkedListAppend.Node(5);
+        System.out.println(node.data + " " + node.next);
 
+        LinkedListAppend linkedList = new LinkedListAppend(5);
+
+        System.out.println(linkedList.head.data);
+        linkedList.append(12);
+        linkedList.append(8);
+
+        linkedList.printAll();
     }
 
 }
