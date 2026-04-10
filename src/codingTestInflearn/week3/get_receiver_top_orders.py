0bf5f29e3cbcdb79b1edf44fe670f2784e504c4d
jjii320 <chwonhan00@gmail.com>
2026-04-10 23:57:25 +0900

    codingTestInflearn Week3 - 스택 개선 파이썬

--- src/codingTestInflearn/week3/get_receiver_top_orders.py
+++ src/codingTestInflearn/week3/get_receiver_top_orders.py
@@ -2,13 +2,16 @@ top_heights = [6, 9, 5, 7, 4]
 
 
 def get_receiver_top_orders(heights):
+    stack = []  # [인덱스, 높이]를 저장
     answer = [0] * len(heights)
-    while heights:
-        height = heights.pop()
-        for idx in range(len(heights) - 1, -1, -1):
-            if height <= heights[idx]:
-                answer[len(heights)] = idx + 1
-                break
+
+    for i in range(len(heights)):
+        while stack and stack[-1][1] < heights[i]:
+            stack.pop()
+        if stack:
+            answer[i] = stack[-1][0] + 1
+        stack.append([i, heights[i]])
+
     return answer
 
 
