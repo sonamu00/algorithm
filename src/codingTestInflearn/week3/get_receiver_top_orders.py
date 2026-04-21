f259ce706e59324026f3aac651ba47cba6026a48
jjii320 <chwonhan00@gmail.com>
2026-04-21 23:57:31 +0900

    test

--- src/codingTestInflearn/week3/get_receiver_top_orders.py
+++ src/codingTestInflearn/week3/get_receiver_top_orders.py
@@ -15,8 +15,7 @@ def get_receiver_top_orders(heights):
     return answer
 
 
-print(get_receiver_top_orders(top_heights))  # [0, 0, 2, 2, 4] 가 반환되어야 한다!
-
+print(get_receiver_top_orders(top_heights))
 print("정답 = [0, 0, 2, 2, 4] / 현재 풀이 값 = ",get_receiver_top_orders([6,9,5,7,4]))
 print("정답 = [0, 0, 2, 3, 3, 3, 6] / 현재 풀이 값 = ",get_receiver_top_orders([3,9,9,3,5,7,2]))
 print("정답 = [0, 0, 2, 0, 0, 5, 6] / 현재 풀이 값 = ",get_receiver_top_orders([1,5,3,6,7,6,5]))
